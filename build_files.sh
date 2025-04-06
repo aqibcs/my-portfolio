f88fc60bf63046cdda80cf7c6115d8b4702322d5
aqibcs <csaqibshah@gmail.com>
2025-04-06 11:43:41 +0100

    Deleted build_files.sh

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Running migrations..."
-python manage.py migrate
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
